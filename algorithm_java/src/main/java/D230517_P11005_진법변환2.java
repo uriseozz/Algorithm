f1debd90b97ef55c17f8282d94b315a897d5b625
YuriSeo <iyoule@naver.com>
2023-05-18 00:00:49 +0900

    5월 17일 알고리즘 문풀

---
+++ algorithm_java/src/main/java/D230517_P11005_진법변환2.java
@@ -0,0 +1,45 @@
+/*
+기초수학
+
+<문제>
+10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+
+<입력>
+첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
+
+<출력>
+첫째 줄에 10진법 수 N을 B진법으로 출력한다.
+
+<예제입력>
+60466175 36
+
+<예제출력>
+ZZZZZ
+
+ */
+
+import java.util.*;
+
+public class D230517_P11005_진법변환2 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        if (B>1 && B<37) {
+            while (N > 0) {
+                if(N%B > 9) {
+                    sb.append((char) (N%B + 55)); //아스키코드 A = 65 ~ Z = 90
+                } else {
+                    sb.append(N%B);
+                }
+                N /= B;
+            }
+            System.out.println(sb.reverse());
+        }
+    }
+}
---
+++ algorithm_java/src/main/java/D230517_P9613_GCD합.java
@@ -0,0 +1,69 @@
+/*
+기초수학
+
+<문제>
+양의 정수 n개가 주어졌을 때, 가능한 모든 쌍의 GCD의 합을 구하는 프로그램을 작성하시오.
+
+<입력>
+첫째 줄에 테스트 케이스의 개수 t (1 ≤ t ≤ 100)이 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있다. 각 테스트 케이스는 수의 개수 n (1 < n ≤ 100)가 주어지고, 다음에는 n개의 수가 주어진다. 입력으로 주어지는 수는 1,000,000을 넘지 않는다.
+
+<출력>
+각 테스트 케이스마다 가능한 모든 쌍의 GCD의 합을 출력한다.
+
+<예제입력>
+3
+4 10 20 30 40
+3 7 5 12
+3 125 15 25
+
+<예제출력>
+70
+3
+35
+ */
+
+import java.util.*;
+
+public class D230517_P9613_GCD합 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+
+        for (int i=0; i<num1; i++) {
+            int num2 = sc.nextInt();
+            int []arr = new int[num2];
+            long sum = 0;
+
+            for (int n=0; n<num2; n++) {
+                arr[n] = sc.nextInt();
+            }
+
+            for (int j=0; j<num2; j++) {
+                for (int r = j; r < num2; r++) {
+                    if(j != r) {
+                        sum += gcd(arr[j], arr[r]);
+                    }
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+
+    // 유클리드 호제법
+    public static int gcd(int a, int b) {
+        if (a<b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b!=0){
+            int r = a%b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+}
