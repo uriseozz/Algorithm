d898601ea858d7c0bfcb445d8165994b6011badf
YuriSeo <iyoule@naver.com>
2023-06-10 00:11:01 +0900

    6월 10일 알고리즘 문풀

---
+++ algorithm_python/D230609_P120824_짝수홀수개수.py
@@ -0,0 +1,15 @@
+def solution(num_list):
+    answer = [0,0]
+
+    for num in num_list:
+        if num % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+    return answer
+
+# 다른 사람의 풀이
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
